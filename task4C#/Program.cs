b586e7d6e0aa7e006d78034fe08d60673b5a6ee7
Evgeny <beloysov_jenya@inbox.ru>
2023-01-20 00:29:32 +0300

    edit task4

--- task4C#/Program.cs
+++ task4C#/Program.cs
@@ -2,11 +2,23 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+// System.Console.WriteLine("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 0; i <= N; i++)
+// {
+//     if (i % 2 == 0)
+//     System.Console.WriteLine(i);
+// }
+
+// МОЖНО УПРОСТИТЬ:
+
 System.Console.WriteLine("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 0; i <= N; i++)
+for (int i = 2; i <= N; i += 2)
 {
-    if (i % 2 == 0)
     System.Console.WriteLine(i);
 }
+    
+    
